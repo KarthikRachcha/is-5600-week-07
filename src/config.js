a90ee268ce411b12894da974e9ea69e8d11b4c4e
KarthikRachcha <karthik.rachcha@slu.edu>
2024-12-09 02:57:30 +0000

    week 5 submission

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // TODO: Replace this with your own Server URL for your codespace
-export const BASE_URL = "https://glowing-space-memory-q7p4jvr9g4p424rj7-3080.app.github.dev";
+export const BASE_URL = "https://laughing-goggles-v6q57w9gpvxg3xvq6-3080.app.github.dev";
